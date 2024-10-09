1eb50f945b26de38feb2254558ae1a3adb02742d
Gurusharan Sahu <175914992+shrnx@users.noreply.github.com>
2024-10-09 07:42:25 +0530

    added a slider

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 let nod;
 let pass;
-let finalPass="";
-let str="Vq5-}{Yu><@3Fg#LkE.DGh0;McB2Ow!T\[pN^4Hi*aKmPzWs%rSU1ZAn,]|&IJ/7'_(jCl8tRbQdX";
-nod=20;
+let str="Vq5-}{Yu><@3Fg#LkE.DGh0;McB2Ow!T[pN^4Hi*aKmPzWs%rSU1ZAn,]|&IJ/7'_(jCl8tRbQdX";
 
-function addInput() {
+function addInput_ChangeSliderValue() {
     let finalPass="";           //Isse Next time jab generate pe click karenge to purana paasword will be removed.
-    for(let i=1;i<=nod;i++) {
-    let a=Math.random();
-    let b=Math.floor(a*str.length);
-    finalPass+=str[b];
+    let passwordLength = slider.value;
+
+    for(let i=1;i<=passwordLength;i++) {
+        let a=Math.random();
+        let b=Math.floor(a*str.length);
+        finalPass+=str[b];
+        }
+
     output.innerText = finalPass;
-}
-    
+
+    setTimeout(() => {
+        generate.disabled = false; // This will disable the button till the password is generated(Page Refresh issue nahi hoga).
+    }, 300);
 }
 
 let output = document.querySelector("#output");
 let generate = document.querySelector("button");
-generate.addEventListener("click",addInput);
 
-console.dir(output);
+let slider = document.querySelector("#slider");
+let sliderValueDisplay = document.querySelector("#SliderValue");
+sliderValueDisplay.innerText = "Password Length = " + slider.value;
+
+
+generate.addEventListener("click",addInput_ChangeSliderValue);
+
+slider.addEventListener("input", () => {
+    sliderValueDisplay.innerText = "Password Length = " + slider.value
+})
+
+
+
