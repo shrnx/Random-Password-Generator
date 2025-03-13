cf5202380a31a1e3c47a12515065073e7e97d210
shrnx <sharansahu1604@gmail.com>
2025-03-14 00:57:19 +0530

    added copy button

--- app.js
+++ app.js
@@ -1,37 +1,60 @@
 let nod;
 let pass;
-let str="Vq5-}{Yu><@3Fg#LkE.DGh0;McB2Ow!T[pN^4Hi*aKmPzWs%rSU1ZAn,]|&IJ/7'_(jCl8tRbQdX";
+let str = "Vq5-}{Yu><@3Fg#LkE.DGh0;McB2Ow!T[pN^4Hi*aKmPzWs%rSU1ZAn,]|&IJ/7'_(jCl8tRbQdX";
 
 function addInput_ChangeSliderValue() {
-    let finalPass="";           //Isse Next time jab generate pe click karenge to purana paasword will be removed.
+    let finalPass = "";           //Isse Next time jab generate pe click karenge to purana paasword will be removed.
     let passwordLength = slider.value;
 
-    for(let i=1;i<=passwordLength;i++) {
-        let a=Math.random();
-        let b=Math.floor(a*str.length);
-        finalPass+=str[b];
-        }
+    for (let i = 1; i <= passwordLength; i++) {
+        let a = Math.random();
+        let b = Math.floor(a * str.length);
+        finalPass += str[b];
+    }
 
-    output.innerText = finalPass;
+    output.textContent = finalPass; // Used textContent instead of innerText as it's slighlty faster
 
     setTimeout(() => {
         generate.disabled = false; // This will disable the button till the password is generated(Page Refresh issue nahi hoga).
     }, 300);
 }
 
-let output = document.querySelector("#output");
-let generate = document.querySelector("button");
+let output = document.getElementById("output");
+let generate = document.getElementById("generate");
 
-let slider = document.querySelector("#slider");
-let sliderValueDisplay = document.querySelector("#SliderValue");
+let slider = document.getElementById("slider");
+let sliderValueDisplay = document.getElementById("SliderValue");
 sliderValueDisplay.innerText = "Password Length = " + slider.value;
 
 
-generate.addEventListener("click",addInput_ChangeSliderValue);
+generate.addEventListener("click", addInput_ChangeSliderValue);
 
 slider.addEventListener("input", () => {
     sliderValueDisplay.innerText = "Password Length = " + slider.value
 })
 
+let upperCase = document.createElement("button")
+
+let copyBtn = document.getElementById("copy")
+
+copyBtn.addEventListener("click", copyPassword);
+
+let copiedPassword;
+
+function copyPassword() {
+    if (output.textContent) {     // Again Used textContent instead of innerText as it's slighlty faster
+        navigator.clipboard.writeText(output.textContent);
+        copyBtn.style.backgroundColor = "green";
+        
+        setTimeout(()=> {
+            copyBtn.style.backgroundColor = "black";
+        },1000)
+
+        console.log("Password Copied");
+    } else {
+        alert("Please generate a password");
+        return;
+    }
+}
 
 
